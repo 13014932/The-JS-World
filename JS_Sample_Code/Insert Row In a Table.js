63dfcce4acc796d7ee4ec9a411677ce870eaee6d
13014932 <ersatpal127@gmail.com>
2018-05-28 00:41:55 +0530

    Insert Row In a Table

---
+++ JS_Sample_Code/Insert Row In a Table.js
@@ -0,0 +1,16 @@
+<script>
+function insert_Row(){
+		
+        var a=document.getElementById("sampleTable").insertRow(2);
+    
+        var b=a.insertCell();
+        var c=a.insertCell();
+        
+        b.innerHTML="new row1 new cell1";
+        c.innerHTML="new row1 new cell2";
+      	
+  
+
+}
+
+</script>
