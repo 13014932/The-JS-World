3b6a53beac05226cb51f758db4ec5285a145b9ec
13014932 <ersatpal127@gmail.com>
2018-05-28 00:38:52 +0530

    Specified number of elements with pre-filled numeric

---
+++ JS_Sample_Code/Specified number of elements with pre-filled numeric.js
@@ -0,0 +1,14 @@
+<script>
+function myFunction() {
+   
+    var a=6;
+    var b=2;
+    var temp=[];
+    for(var i=0;i<a;i++){
+    
+    	temp.push([b]);
+    }
+ 
+    document.getElementById("demo").innerHTML=temp;
+}
+</script>
